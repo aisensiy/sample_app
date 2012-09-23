a05925c961a3e61e8c62caf67fe0da41478a9f20
aisensiy <aisensiy@gmail.com>
2012-09-23 19:54:02 +0800

    chpt5 finish

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe "full_title" do
+    it "should include page title" do
+      full_title('foo').should =~ /foo$/
+    end
+    it "should include base title" do
+      full_title('foo').should =~ /^Ruby on Rails Tutorial Sample App/
+    end
+    it "should not include '|' when page title is ''" do
+      full_title('').should_not =~ /\|/
+    end
+  end
+end
